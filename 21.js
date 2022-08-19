c3523c61c65b856044bf161923d71223960a929c
example@gmail.com <gotic@list.ru>
2022-08-19 23:04:11 +0300

    add new lessons

---
+++ 21.js
@@ -0,0 +1,86 @@
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//   console.log('Я сыт!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'wukgflskawjghrl');
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//   console.log('все сыты');
+// } else {
+//   console.log('Мы уходим');
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//   console.log('все сыты');
+// } else {
+//   console.log('Мы уходим');
+// }
+
+// console.log(hamburger || cola || fries);
+
+// let johnReport, alekReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alekReport || samReport || mariaReport);
+
+ 
+// console.log( NaN || 2 || undefined );
+ 
+// console.log( NaN && 2 && undefined );
+ 
+// console.log( 1 && 2 && 3 );
+ 
+// console.log( !1 && 2 || !3 );
+ 
+// console.log( 25 || null && !3 );
+ 
+// console.log( NaN || null || !3 || undefined || 5);
+ 
+// console.log( NaN || null && !3 && undefined || 5);
+ 
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!');
+// }
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}
