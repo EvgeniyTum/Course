476b8883ac2a6d51301f9c00514140d1e987d963
example@gmail.com <gotic@list.ru>
2022-09-13 17:03:05 +0300

    add lessons 33 & 35. Use test html & js

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
-function fib(n) {
-  if (n < 1 || typeof(n) !== 'number') {
-    return '';
+const someString = 'This is some strange string';
+
+function reverse(str) {
+  if (typeof(str) !== 'string' || str.length === 0) {
+    return 'Ошибка';
   }
-  let result = [];
-  for (let i = 1; i <= n; i++) {
-    if (i < 3) {
-      result.push(i - 1);
-    } else {
-      result.push(result[i - 3] + result[i - 2]);
-    }
-  }
-  return result.join(' ');
+  return str.split('').reverse().join('');
 }
 
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-console.log(fib(5));
-console.log(fib(10));
+console.log(reverse(someString));
