2d4a64c3c08e1d1084abbb4071a1112df7f52851
example@gmail.com <gotic@list.ru>
2022-09-16 22:00:48 +0300

    add lesson 38 & use test.js and index.html

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
-const someString = 'This is some strange string';
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi'];
 
-function reverse(str) {
-  if (typeof(str) !== 'string' || str.length === 0) {
-    return 'Ошибка';
-  }
-  return str.split('').reverse().join('');
+function sortStudentsByGroups(arr) {
+    let sortStudents = arr.sort();
+    const result = [];
+    for (let i = 0; i < 4; i++) {
+        if (i === 3 && sortStudents.length !== 0) {
+            result.push(`Оставшиеся студенты: ${sortStudents.join(', ')}`);
+            break;
+        } if (i === 3 && sortStudents.length === 0) {
+            result.push(`Оставшиеся студенты: -}`);
+            break;
+        } 
+        let group = [];
+        for (let j = 0; j < 3; j++) {
+            group.push(sortStudents[j]);
+        }
+        result.push(group);
+        sortStudents = sortStudents.slice(3);
+    }
+    return result;
 }
 
-console.log(reverse(someString));
+console.log(sortStudentsByGroups(students));
